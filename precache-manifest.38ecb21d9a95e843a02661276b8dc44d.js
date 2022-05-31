43a8c49752e3a5c622b78228f4964ec5720e6444
Dhaval Tank <76875509+DhavalTank01@users.noreply.github.com>
2022-05-31 10:26:56 +0530

    Deploy React app to GitHub Pages

---
+++ precache-manifest.38ecb21d9a95e843a02661276b8dc44d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "81934557dd6347be834c",
+    "url": "/api-demo/static/css/main.eb27c535.chunk.css"
+  },
+  {
+    "revision": "81934557dd6347be834c",
+    "url": "/api-demo/static/js/main.4a0f1197.chunk.js"
+  },
+  {
+    "revision": "8d5defd29b8fc8ed780e",
+    "url": "/api-demo/static/js/runtime~main.aff8f8ec.js"
+  },
+  {
+    "revision": "5b07ac52e0113c5d2e46",
+    "url": "/api-demo/static/js/2.2c738fd2.chunk.js"
+  },
+  {
+    "revision": "84f74231df77064d8db50ed9e252c5c5",
+    "url": "/api-demo/index.html"
+  }
+];
